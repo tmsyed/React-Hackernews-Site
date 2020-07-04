10902bc7a863ccc4bc2e696d2f93ddda77d5c33d
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2020-07-04 18:32:18 -0400

    Added states and hooks to components
    Moved the list array to inside the App component
    
    Used props to pass data
    
    Updated search term functionality to use an onChange handler and a hook to manafe the state of the search bar

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,45 +1,54 @@
 import React from 'react';
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
 
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+  
+    },
+  ];
 
-function App() {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+      <div>
+        <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={handleChange} />
 
-      <hr />
-      
-      {/*list.map((item) => <div key={item.objectID}>{item.title}</div>)*/}
-      <List />
-    </div>
+        <p>
+          Searching for <strong>{searchTerm}</strong>.
+        </p>
+
+        <hr />
+        
+        {/*list.map((item) => <div key={item.objectID}>{item.title}</div>)*/}
+        <List list={stories} />
+      </div>
   );
 }
 
-function List() {
-  return list.map(function(item) {
-    return (
+const List = props =>
+  props.list.map(item => (
       <div key={item.objectID}>
         <span>
           <a href={item.url}>{item.title}</a>
@@ -48,8 +57,7 @@ function List() {
         <span>{item.num_comments}</span>
         <span>{item.points}</span>
       </div>
-    );
-  });
-}
+  ));
+
 
 export default App;
