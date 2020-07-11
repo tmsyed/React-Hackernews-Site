98438fb57a5b32d6fbaf2c4c9e4596e90792cf94
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2020-07-10 20:25:54 -0400

    Grouped states together with useReducer
    All states belonging to managing stories is part of the storiesReducer

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,5 +1,67 @@
 import React from 'react';
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+
+  },
+];
+
+const storiesReducer = (state, action) => {
+  switch(action.type) {
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      }
+    case "REMOVE_STORY":
+      return {
+        ...state,
+        data: state.data.filter(
+          story => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const getAsyncStories = () => 
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve({ data: {stories: initialStories} }),
+      2000
+    ) 
+);
+
+
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
     localStorage.getItem(key) || initialState
@@ -13,33 +75,39 @@ const useSemiPersistentState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-  
-    },
-  ];
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer,
+    { data: [], isLoading: false, isError: false }
+  );
+
+  React.useEffect(() => {
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
+
+    getAsyncStories()
+      .then(result => {
+        dispatchStories({
+          type: "STORIES_FETCH_SUCCESS",
+          payload: result.data.stories
+        });
+      })
+      .catch(() => 
+        dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+  }, []);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
   };
+
+  const handleRemoveStory = item => {
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: item,
+    });
+  };
   
-  const searchedStories = stories.filter(story => 
+  const searchedStories = stories.data.filter(story => 
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -52,9 +120,16 @@ const App = () => {
         </InputWithLabel>
 
         <hr />
+
+        {stories.isError && <p>Something went wrong...</p>}
+
+        {stories.isLoading ? (
+          <p>Loading...</p>
+        ): (
+          <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        )}
         
         {/*list.map((item) => <div key={item.objectID}>{item.title}</div>)*/}
-        <List list={searchedStories} />
       </div>
   );
 }
@@ -78,11 +153,13 @@ const InputWithLabel = ({ id, type="text", value, isFocused, onInputChange, chil
 }
 
 
-const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
+const List = ({ list, onRemoveItem }) =>
+  list.map(item => 
+    <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+  );
 
 
-const Item = ({ item }) => (
+const Item = ({ item, onRemoveItem }) => (
   <div>
     <span>
       <a href={item.url}>{item.title}</a>
@@ -90,6 +167,11 @@ const Item = ({ item }) => (
     <span>{item.author}</span>
     <span>{item.num_comments}</span>
     <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
   </div>
 );
 export default App;
