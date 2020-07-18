3c97a6f9c1df7781a7c12c2c68f43129c16ceaf3
Tasnim Syed <45466028+tmsyed@users.noreply.github.com>
2020-07-18 17:15:03 -0400

    Used CSS module instead of regular CSS
    Also included classnames package for easier class name usage

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-
-import './App.css';
+import cs from 'classnames';
+import styles from './App.module.css';
 
 const storiesReducer = (state, action) => {
   switch(action.type) {
@@ -100,8 +100,8 @@ const App = () => {
   };
 
   return (
-      <div className="container">
-        <h1 className="headline-primary">My Hacker Stories</h1>
+      <div className={styles.container}>
+        <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
 
         <SearchForm
           searchTerm={searchTerm}
@@ -123,7 +123,7 @@ const App = () => {
 };
 
 const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit, }) => (
-  <form onSubmit={onSearchSubmit} className="search-form">
+  <form onSubmit={onSearchSubmit} className={styles.SearchForm}>
     <InputWithLabel id="search" value={searchTerm} isFocused onInputChange={onSearchInput}>
       <strong>Search:</strong>
     </InputWithLabel>
@@ -131,7 +131,7 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit, }) => (
     <button
       type="submit"
       disabled={!searchTerm}
-      className="button button_large">
+      className={cs(styles.button, styles.buttonLarge)}>
         Submit
     </button>
   </form>
@@ -148,7 +148,7 @@ const InputWithLabel = ({ id, type="text", value, isFocused, onInputChange, chil
 
   return (
       <>
-        <label htmlFor={id} className="label">
+        <label htmlFor={id} className={styles.label}>
           {children}
         </label>
         &nbsp;
@@ -158,7 +158,7 @@ const InputWithLabel = ({ id, type="text", value, isFocused, onInputChange, chil
           type={type} 
           value={value} 
           onChange={onInputChange} 
-          className="input"
+          className={styles.input}
         />
       </>
 );
@@ -172,7 +172,7 @@ const List = ({ list, onRemoveItem }) =>
 
 
 const Item = ({ item, onRemoveItem }) => (
-  <div className="item">
+  <div className={styles.item}>
     <span style={{ width: '40%' }}>
       <a href={item.url}>{item.title}</a>
     </span>
@@ -180,7 +180,7 @@ const Item = ({ item, onRemoveItem }) => (
     <span style={{ width: '10%' }}>{item.num_comments}</span>
     <span style={{ width: '10%' }}>{item.points}</span>
     <span style={{ width: '10%' }}>
-      <button type="button" className="button button_small" onClick={() => onRemoveItem(item)}>
+      <button type="button" className={`${styles.button} ${styles.buttonSmall}`} onClick={() => onRemoveItem(item)}>
         Dismiss
       </button>
     </span>
